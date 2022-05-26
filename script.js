7f4d7db2abbbe5f0352a72c04c823c084b335475
Julia Tselicova <malipuha@list.ru>
2022-05-26 20:15:04 +1000

    домашка по певому уроку

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const title = "title";
+const title = "world";
 const screens = "screens";
 const screenPrice = "screenPrice";
 const rollback = "rollback";
 const fullPrice = "fullPrice";
 const adaptive = "adaptive";
+
+alert( "Hello" + title);
+console.log(adaptive);
