1c7c83e754e31f4fd9a7c2e700d3deffdd2813e7
Julia Tselicova <malipuha@list.ru>
2022-05-30 18:51:00 +1000

    новая ветка харда

--- hard/scriptHard.js
+++ hard/scriptHard.js
@@ -1,1 +1,16 @@
-console.log("hard hello");
+let num = 266219;
+
+let arrNum = String(num).split('');
+
+function multiplication(){
+ 
+        result = arrNum.reduce(function(prod, current){
+        return  prod * current;
+
+        } )
+}
+multiplication();
+
+
+
+console.log(String(result ** 3).substr(0,2));
