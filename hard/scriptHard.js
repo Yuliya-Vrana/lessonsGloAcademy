4c34ba215a9be5fc121ebfb4eab7ce7ab1a38311
Julia Tselicova <malipuha@list.ru>
2022-05-30 22:27:18 +1000

    усложненная версия домашней работы по третьему уроку

--- hard/scriptHard.js
+++ hard/scriptHard.js
@@ -1,16 +1,44 @@
-let num = 266219;
+// let num = 266219;
+// let arrNum = String(num).split('');
 
-let arrNum = String(num).split('');
+// function multiplication(){
+//        result = arrNum.reduce(function(prod, current){
+//        return  prod * current;
+//        } )
+// }
+// multiplication();
+// console.log(String(result ** 3).substr(0,2));
 
-function multiplication(){
- 
-        result = arrNum.reduce(function(prod, current){
-        return  prod * current;
+const lang = 'ru';
 
-        } )
-}
-multiplication();
+if (lang == 'en'){
+    console.log("Английский дни недели");
+}else 
+    console.log("Русские дни недели");
 
 
+switch (lang) {
+    case 'en':
+        console.log("Английский дни недели");
+        break;
+      
+    case 'ru':   
+        console.log("Русские дни недели");
+        break;
+      
+        default:
+            console.log("Русские дни недели");
+      }    
 
-console.log(String(result ** 3).substr(0,2));
+const lang_array = [];
+lang_array ['ru'] = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+lang_array ['en'] = ['Mo', 'Te', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+
+console.log(lang_array[lang]);
+
+
+const namePerson = "Александр";
+
+namePerson == "Артём" ? console.log ("Директор") : namePerson == "Александр" ? console.log ("Преподаватель") : console.log ("Студент");
+
+
