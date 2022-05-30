9b40ac5abc4216f39f46aa4c7ac45a5eed214e4c
Julia Tselicova <malipuha@list.ru>
2022-05-30 18:01:32 +1000

    домашнее задание по уроку 3

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-const title = "world";
-const screens = "screens";
-const screenPrice = "screenPrice";
-const rollback = "rollback";
-const fullPrice = "fullPrice";
-const adaptive = "adaptive";
+'use strict';
 
-alert( "Hello" + title);
+const title = prompt('Как называется Ваш проэкт?');
+const screens = prompt ('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+const screenPrice = prompt ('Сколько будет стоить данная работа?');
+const rollback = "99";
+const adaptive = confirm ('Нужен ли адаптив на сайте?');
+
+console.log(title);
+console.log(screens);
+console.log(parseInt(screenPrice));
 console.log(adaptive);
+
+const servise1 = prompt('Какой дополнительный тип услуги нужен?');
+const servisePrice1= prompt('Сколько это будет стоить?');
+const servise2 = prompt('Какой дополнительный тип услуги нужен?');
+const servisePrice2 = prompt('Сколько это будет стоить?');
+
+const fullPrice = parseFloat(screenPrice) +  parseFloat(servisePrice1) + parseFloat(servisePrice2);
+
+console.log(fullPrice);
+
+const servicePercentPrice = fullPrice * (rollback/100);
+
+console.log(Math.ceil (servicePercentPrice));
+
+if (fullPrice >= 30000){
+    alert ('Даём скидку в 10%');
+}else if (15000 <= fullPrice){
+        alert ('Даём скидку в 5%');
+   }else if (0 <= fullPrice){
+        alert('Скидка не предусмотрена');
+     }else
+        alert('Что то пошло не так');
+
+// console.log(title + ', ' + fullPrice + ', ' + adaptive);
+// console.log(screens.length);
+// console.log("Стоимость вёрстки экранов " + screenPrice + " рублей");
+// console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+// newScreens = screens.toLowerCase();
+// console.log(newScreens.split(", "));
+
+// console.log(fullPrice * (rollback/100));
