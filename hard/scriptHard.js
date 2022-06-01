87dfa78d4332797af8ac6f1992e438b1d7651d4e
Julia Tselicova <malipuha@list.ru>
2022-06-01 18:31:04 +1000

    усложнённое домашнее задание 4

--- hard/scriptHard.js
+++ hard/scriptHard.js
@@ -1,16 +1,24 @@
-let num = 266219;
+const num = 266219;
+const letter = prompt('write');
 
-let arrNum = String(num).split('');
-
-function multiplication(){
- 
+const multiplication = function(){
+        let arrNum = String(num).split('');
         result = arrNum.reduce(function(prod, current){
         return  prod * current;
 
         } )
 }
+
+const myString = function (str){
+        if (typeof str !=="string"){
+                alert ("no string");
+                return;
+        }else       
+                str = str.trim();  
+                return str.length > 30 ? str.slice(0, 30) + '...' : str;         
+}
+
 multiplication();
 
-
-
 console.log(String(result ** 3).substr(0,2));
+console.log(myString(letter));
