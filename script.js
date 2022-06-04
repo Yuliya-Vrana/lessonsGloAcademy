cdcc0a3cb74f30489ef094eee8b550b16e2849a8
Julia Tselicova <malipuha@list.ru>
2022-06-04 21:33:34 +1000

    домашка game

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 'use strict';
 
 function one (x){
+    const hiddenNumber = Math.ceil(Math.random() * 100)
+    console.log (hiddenNumber)
+
+
     function two (){
-        const answer = +prompt('Введите число')
+        const userNumber = prompt('Я загадал число от 1 до 100. Попробуй, угадай какое. Введи число!')
         x--
+        console.log(typeof userNumber, userNumber)
+        if(userNumber == ''){
+            alert ('Всё-таки число нужно ввести')
+            two()
+        }else(userNumber === null)
+            alert ('Игра окончена. До встречи!')
+        
 
-        console.log(answer);
-        console.log(x);
-
-        // if (answer > x) {
-        //     alert ('Моё число меньше')
-        //     two()
-        // }else if(answer < x){
-        //     alert ('Моё число меньше')
-        //     two()
-        // }else
-        //     alert ("вы угадали!")
-    }
+        if (parseInt(userNumber) < hiddenNumber) {
+            alert ('Моё число больше')
+            two()
+        }else if(parseInt(userNumber)  > hiddenNumber){
+            alert ('Моё число меньше')
+            two()
+        }else if(parseInt(userNumber)  == hiddenNumber){
+            alert ('Ура! Ты угадал(-а)!')
+        }
+     }
+    two()
 }
 
-one (10)
+one(10)
+
+
