ebe3d08ac0065a50de4fa67caa3c954b02b0e461
Julia Tselicova <malipuha@list.ru>
2022-06-04 21:34:16 +1000

    домашка game красивая

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 'use strict';
 
-function one (x){
+function one (){
     const hiddenNumber = Math.ceil(Math.random() * 100)
     console.log (hiddenNumber)
 
 
     function two (){
         const userNumber = prompt('Я загадал число от 1 до 100. Попробуй, угадай какое. Введи число!')
-        x--
+        
         console.log(typeof userNumber, userNumber)
-        if(userNumber == ''){
+        if(userNumber === ''){
             alert ('Всё-таки число нужно ввести')
             two()
         }else(userNumber === null)
             alert ('Игра окончена. До встречи!')
         
-
         if (parseInt(userNumber) < hiddenNumber) {
             alert ('Моё число больше')
             two()
@@ -29,6 +28,6 @@ function one (x){
     two()
 }
 
-one(10)
+one()
 
 
