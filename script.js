40c8ae8e1efe085ec6b029eae5159652bd14e35e
Julia Tselicova <malipuha@list.ru>
2022-06-04 22:31:14 +1000

    усложнённая домашка game

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
 'use strict';
+let attempt = 5;
 
-function one (){
+function one (n){
     const hiddenNumber = Math.ceil(Math.random() * 100)
-    console.log (hiddenNumber)
+       
+        function two (){
+            const userNumber = prompt('Я загадал число от 1 до 100. Попробуй, угадай какое. Введи число!')
+            n--
+          
+            if (n > 0){
 
+                if(userNumber == ''){
+                    alert ('Всё-таки число нужно ввести')
+                    two()
+                }else if (isNaN(userNumber) && !isFinite(userNumber)){
+                    alert ('Число нужно ввести')
+                     two()
+                }else if (userNumber == null){
+                    alert ('Игра окончена. До встречи!')
+                }
+               
+                if (parseInt(userNumber) < hiddenNumber) {
+                    alert ('Моё число больше. Осталось ' + n + ' попыток')
+                    two()
+                }else if(parseInt(userNumber)  > hiddenNumber){
+                    alert ('Моё число меньше. Осталось ' + n + ' попыток')
+                    two()
+                }else if(parseInt(userNumber)  == hiddenNumber){
+                    alert ('Ура! Ты угадал(-а)!')
+                }
+            }else if (n == 0){
+                let newGame = confirm ('Попытки закончились. Сыграем ещё раз?')
 
-    function two (){
-        const userNumber = prompt('Я загадал число от 1 до 100. Попробуй, угадай какое. Введи число!')
-        
-        console.log(typeof userNumber, userNumber)
-        if(userNumber === ''){
-            alert ('Всё-таки число нужно ввести')
-            two()
-        }else(userNumber === null)
-            alert ('Игра окончена. До встречи!')
-        
-        if (parseInt(userNumber) < hiddenNumber) {
-            alert ('Моё число больше')
-            two()
-        }else if(parseInt(userNumber)  > hiddenNumber){
-            alert ('Моё число меньше')
-            two()
-        }else if(parseInt(userNumber)  == hiddenNumber){
-            alert ('Ура! Ты угадал(-а)!')
+                    if (newGame == true){
+                        one(attempt)
+                    }else 
+                        alert ('До встречи!')
+            }
         }
-     }
-    two()
+    two() 
 }
 
-one()
+one(attempt)
 
 
