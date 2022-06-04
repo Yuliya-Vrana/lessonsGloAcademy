d19bda6bb929d20f98084b3188d2343d3c21d782
Julia Tselicova <malipuha@list.ru>
2022-06-04 20:13:56 +1000

    домашка game

--- script.js
+++ script.js
@@ -1,50 +1,22 @@
 'use strict';
 
-const title = prompt('Как называется Ваш проэкт?');
-const screens = prompt ('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-const screenPrice = +prompt ('Сколько будет стоить данная работа?');
-const rollback = "10";
-const adaptive = prompt ('Нужен ли адаптив на сайте?');
-const servise1 = prompt('Какой дополнительный тип услуги нужен?');
-const servisePrice1= + prompt('Сколько это будет стоить?');
-const servise2 = prompt('Какой дополнительный тип услуги нужен?');
-const servisePrice2 = + prompt('Сколько это будет стоить?');
+function one (x){
+    function two (){
+        const answer = +prompt('Введите число')
+        x--
 
-const showTipeOf = function (variable){
-    console.log (variable, typeof variable);
-}
-const fullPrice = function getFullPrice(){
-    return parseFloat(screenPrice) + allServicePrices()
-}
+        console.log(answer);
+        console.log(x);
 
-const allServicePrices = function getAllServicePrices (){
-    return parseFloat(servisePrice1) + parseFloat(servisePrice2)
+        // if (answer > x) {
+        //     alert ('Моё число меньше')
+        //     two()
+        // }else if(answer < x){
+        //     alert ('Моё число меньше')
+        //     two()
+        // }else
+        //     alert ("вы угадали!")
+    }
 }
 
-const getRollbackMessage = function (price){
-    if (price >= 30000){
-        return 'Даём скидку в 10%';
-    }else if (price >= 15000 && price < 30000){
-            return 'Даём скидку в 5%';
-    }else if (price >= 0 && price < 15000){
-                return 'Скидка не предусмотрена';
-        }else
-            return 'Что то пошло не так';
-}
-
-const getTitle = function(str){
-    let newStr = str.replace(/^ /,"");
-    return newStr[0].toUpperCase() + newStr.slice(1); 
-}
-
-const servicePercentPrice = function getServicePercentPrices(price){
-    return price * (rollback/100)
-}
-showTipeOf(title, screenPrice, adaptive);
-
-console.log(fullPrice());
-console.log(allServicePrices());
-console.log(getTitle(title));
-console.log(getRollbackMessage(fullPrice()));
-console.log(servicePercentPrice(fullPrice()));
-
+one (10)
