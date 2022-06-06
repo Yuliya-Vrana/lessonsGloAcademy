0eb6dfe68a0841f20eed7a32dccf8eb067eec4f2
Julia Tselicova <malipuha@list.ru>
2022-06-06 16:23:53 +1000

    домашнее усложнённое по уроку 7

--- hard/scriptHard.js
+++ hard/scriptHard.js
@@ -1,79 +1,64 @@
-<<<<<<< HEAD
-// let num = 266219;
-// let arrNum = String(num).split('');
-
-// function multiplication(){
-//        result = arrNum.reduce(function(prod, current){
-//        return  prod * current;
-//        } )
-// }
-// multiplication();
-// console.log(String(result ** 3).substr(0,2));
-
-const lang = 'ru';
-
-if (lang == 'en'){
-    console.log("Английский дни недели");
-}else 
-    console.log("Русские дни недели");
-
-
-switch (lang) {
-    case 'en':
-        console.log("Английский дни недели");
-        break;
-      
-    case 'ru':   
-        console.log("Русские дни недели");
-        break;
-      
-        default:
-            console.log("Русские дни недели");
-      }    
-
-const lang_array = [];
-lang_array ['ru'] = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-lang_array ['en'] = ['Mo', 'Te', 'We', 'Th', 'Fr', 'Sa', 'Su'];
-
-console.log(lang_array[lang]);
-
-
-const namePerson = "Александр";
-
-namePerson == "Артём" ? console.log ("Директор") : namePerson == "Александр" ? console.log ("Преподаватель") : console.log ("Студент");
-=======
 let num = 266219;
 let arrNum = String(num).split('');
+let askingLanguage = prompt('Выбери язык', 'ru')
+let lang_array = [];
+let namePerson = prompt ("Имя", "Александр");
 
-let arrNumbers= ['24569', '458963', '5896314', '4578930', '352487', '24578963', '2578960'];
-
-const multiplication = function (num){
-        result = arrNum.reduce(function(prod, current){
-        return  prod * current;
-        } )
+const multiplication = function(){
+       result = arrNum.reduce(function(prod, current){
+       return  prod * current;
+       } )
 }
 
-const getNumber = function (arr){
-        let newArr = []
-        arr.forEach((item) => {if (item.startsWith ('2') || item.startsWith ('4')) newArr.push(item)}); 
-        return (newArr)
+const languageIf = function(lang){
+        if (lang == 'en'){
+                return("Английский дни недели")
+        }else 
+                return("Русские дни недели")
 }
- const showSimpleNumber  = function (n){
-        if(n>1){
-        nextStep:
-            for (let i = 2; i <= n; i++) {
-             for (let j = 2; j < i; j++) {
-                if(i % j == 0){
-                   continue nextStep
-                }
-             }
-             console.log ( i + '- это простое число. Делители этого числа: 1 и ' + i )
-            } 
+
+const languageSwitch = function(lang){
+        switch (lang) {
+        case 'en':
+                return("Английский дни недели");
+                       
+        case 'ru':   
+                return("Русские дни недели");
+                        
+                default:
+                return("Русские дни недели");
+        } 
+}
+
+const getStatus = function(){
+      namePerson == "Артём" ? console.log ("Директор") : namePerson == "Александр" ? console.log ("Преподаватель") : console.log ("Студент");
+}
+
+const getWeek = function (){
+        const week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
+
+        let today = new Date() 
+        let todayDay = +today.getDay()-1
+
+        for (let i = 0; i < week.length; i++) {
+
+                if (i === today.getDay()){
+                        document.write ('<b>' + week[i] + '</b><br>')           
+                }else if(week[i] == 'суббота' || week[i] == 'воскресенье'){
+                        document.write ('<i>' + week[i]+ '</i><br>')    
+                }else 
+                document.write (week[i] +'<br>')
         }
 }
-multiplication();
-showSimpleNumber(100);
 
-console.log(getNumber(arrNumbers));
+lang_array ['ru'] = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+lang_array ['en'] = ['Mo', 'Te', 'We', 'Th', 'Fr', 'Sa', 'Su']; 
+
+multiplication();
+getWeek()
+getStatus()
+
 console.log(String(result ** 3).substr(0,2));
->>>>>>> lesson05Two
+console.log(languageIf(askingLanguage));
+console.log(languageSwitch(askingLanguage));
+console.log(lang_array[askingLanguage]);
