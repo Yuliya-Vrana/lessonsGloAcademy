2dfdc0b2fc6e7c3c65a5016ab04f1e96f5993b4e
Julia Tselicova <malipuha@list.ru>
2022-06-28 01:32:55 +1000

    проверка регулярные выражения

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 import scroll from './modules/scroll'
+import verification from './modules/verification'
 
 timer('25 june 2022')
 menu()
 modal()
 scroll()
+verification()
