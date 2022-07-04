1dc8078755b05559d965c39052766cbe89f7bd17
Julia Tselicova <malipuha@list.ru>
2022-07-04 23:47:40 +1000

    fetch get and post

--- script.js
+++ script.js
@@ -1,126 +1,29 @@
-// let s = new Set();
-// s.add({name: servise, sum:price});
-// for(let i=0; i < 2; i++){
-//     let price = 0
-//     let name
-//         name = prompt('Какой дополнительный тип услуги нужен?','forms')   
-//         price = prompt('Сколько это будет стоить?','1000')     
-//     s.add({id: i, name:name, price:price})
-// }
-// console.log (s)
+const { url } = require("inspector")
 
-'use strict';
-
-const appData = {
-    title: '',
-    screens: [],
-    screenPrice: 0,
-    adaptive: true,
-    allServicePrices: 0,
-    fullPrice: 0,
-    servicePercentPrice: 0,
-    rollback: 10,
-    servises: new Set(),
-    servisePrice: 0,
-
-    isNumber: function (num) {
-        return !isNaN(parseFloat(num)) && isFinite(num) && num != null
-    },
-
-    isString: function(str){
-        return str != null && str !== '' && !isFinite(str)
-    },
-    asking: function () {
-        do{
-            appData.title = prompt('Как называется Ваш проэкт?','калькулятор верстки')     
-        }while(!appData.isString(appData.title))
-        
-        for(let i=0; i < 2; i++){ 
-            let price = 0  
-            let name 
-
-            do{
-                name = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')  
-            }while(!appData.isString(name))
-        
-            do{
-                price = prompt ('Сколько будет стоить данная работа?','15000')     
-            }while(!appData.isNumber(price))
-
-            appData.screens.push ({id: i, name:name, price:price})
-        } 
-           
-        appData.adaptive = prompt ('Нужен ли адаптив на сайте?','да')
-
-        for(let i=0; i < 2; i++){
-            let price = 0
-            let name
-
-            do{
-                name = prompt('Какой дополнительный тип услуги нужен?','forms')   
-            }while(!appData.isString(name))
-   
-            do{
-                price = prompt('Сколько это будет стоить?','1000')     
-            }while(!appData.isNumber(price))
-
-            appData.servises.add({id: i, name:name, price:price})   
-        }
-    },
-
-    addPrices: function(){
-
-        appData.screenPrice = appData.screens.reduce((sum, current) => sum + current.price, 0)
-
-        appData.allServicePrices = Object.values(appData.servises).reduce((sum, current) => sum + current, 0)
-       
-    },
-  
-    getFullPrice: function (){   
-        appData.fullPrice =  parseFloat(appData.screenPrice) + appData.allServicePrices
-    },
-
-    getServicePercentPrice: function (){
-        appData.servicePercentPrice =  appData.fullPrice - (appData.fullPrice * (appData.rollback/100))
-    },
-
-    getTitle: function(){
-        appData.title =  appData.title.trim()[0].toUpperCase() + appData.title.trim().substr(1).toLowerCase(); 
-    },
-    
-    getRollbackMessage: function (price){
-    if (price >= 30000){
-        return 'Даём скидку в 10%';
-    }else if (price >= 15000 && price < 30000){
-            return 'Даём скидку в 5%';
-        }else if (price >= 0 && price < 15000){
-               return 'Скидка не предусмотрена';
-                }else
-                    return 'Что то пошло не так';
-    },
-
-    logger: function(){
-        console.log(appData.title);
-        console.log(appData.screens);
-        console.log(appData.servises);
-        console.log(appData.allServicePrices);
-        console.log(appData.fullPrice);
-        console.log(appData.servicePercentPrice);
-        console.log(appData.getRollbackMessage(appData.fullPrice));
-        // for (let key in appData)
-        //     console.log (key)      
-    },
-
-    start: function(){
-        appData.asking()
-        appData.getTitle()
-        appData.addPrices()
-
-        appData.getFullPrice()
-        appData.getServicePercentPrice()
-
-        appData.logger()      
-    }
+const getData = () => {
+    fetch('db.json')
+        .then(response => response.json())
 }
- 
-appData.start()
+
+const sendData = (url) => {
+    return fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                "user": "Vlad",
+                "age": 33,
+                "role": "developer"
+            }),
+            headers: {
+                'Content-type': 'application/json; charset-UTF-8',
+            },
+    }).then(response => response.json())
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    getData()
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    sendData('https://jsonplaceholder.typicode.com/posts')
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+})
