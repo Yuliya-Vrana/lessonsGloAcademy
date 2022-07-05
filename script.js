d76eee04d534932d089ee318e78a556eef2da92f
Julia Tselicova <malipuha@list.ru>
2022-07-05 15:53:21 +1000

    не меняла, пробовала

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const getData = () => {
-    fetch('heroes/dbHeroes.json')
+    return fetch('heroes/dbHeroes.json')
         .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     getData()
-      // .then(data => console.log(data))
-       // .catch(error => console.log(error))
+       .then(data => console.log(data))
+       .catch(error => console.log(error))
 
 })
